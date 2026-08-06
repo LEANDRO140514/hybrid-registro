5cca8013c806e35e78acfff7b4c189fce7e06a44
Leandro Miguel Espinosa Romero <leandro.m.espinosa@gmail.com>
2026-08-06 05:29:06 -0600

    feat: reabre el sitio - apaga la prepagina y sale a venta

--- src/config/holdingMode.ts
+++ src/config/holdingMode.ts
@@ -2,4 +2,4 @@
 // completo (todas las rutas) mientras se reordenan los bloques del
 // itinerario, y mostrar en su lugar una pantalla de espera con reserva.
 // Para reabrir el sitio normal: cambiar a false.
-export const HOLDING_MODE_ACTIVE = true
+export const HOLDING_MODE_ACTIVE = false
